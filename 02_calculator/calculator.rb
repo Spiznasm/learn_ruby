2fa90a397d9a93186895834c5813ca6bb7334704
Sam Nixa <spiznasm@gmail.com>
2016-11-24 13:20:12 -0600

    Finished 00 to 03

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,38 @@
 #write your code here
+def add(a,b)
+  a+b
+end
+
+def subtract(a,b)
+  a - b
+end
+
+def sum(numberList)
+  total = 0
+  for num in numberList
+    total += num
+  end
+  return total
+end
+
+def multiply(numberList)
+  total = 1
+  for num in numberList
+    total *= num
+  end
+  return total
+end
+
+def power(a,b)
+  a**b
+end
+
+def factorial(num)
+  current = num
+  total = 1
+  while current > 0
+    total *= current
+    current -=1
+  end
+  return total
+end
--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,38 @@
 #write your code here
+def echo(statement)
+  "#{statement}"
+end
+
+def shout(statement)
+  "#{statement.upcase}"
+end
+
+def repeat(statement,repetitions=2)
+  withSpace = "#{statement} "
+  repeated = "#{withSpace*repetitions}"
+  return repeated[0..-2]
+
+end
+
+def start_of_word(statement,num)
+  "#{statement}"[0..num-1]
+end
+
+def first_word(statement)
+  statement.split[0]
+end
+
+def titleize(statement)
+  caps = []
+  for word in statement.split
+
+    if caps.length==0
+      caps << word.capitalize
+    elsif ["and","or","to","over","the"].include? word
+      caps << word.downcase
+    else
+      caps << word.capitalize
+    end
+  end
+  return caps.join(" ")
+end
