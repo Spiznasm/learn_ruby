f3878d9f75dc456471497d5846102978a9b28013
Sam Nixa <spiznasm@gmail.com>
2016-11-26 11:21:50 -0600

    Finished 06

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,17 @@
 class Timer
-  #write your code here
+  attr_accessor :seconds, :minutes, :hours
+  def seconds
+    @seconds = 0
+  end
+
+  def time_string
+    #Option 1
+    #Time.at(@seconds).utc.strftime("%H:%M:%S")
+
+    #Option 2
+    display_seconds = @seconds%60
+    minutes = (@seconds/60)%60
+    hours = @seconds/3600
+    format("%02d:%02d:%02d",hours,minutes,display_seconds)
+  end
 end
