db13a60cba08877eb51e13ed6170087e3fb91b87
Sam Nixa <spiznasm@gmail.com>
2016-11-25 21:38:37 -0600

    Finished 05

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,19 @@
 class Book
-# write your code here
+  attr_accessor :title
+  def title
+    caps = []
+    for word in @title.split
+
+      if caps.length==0
+        caps << word.capitalize
+      elsif ["and","or","to","over","the","of","in","a","an"].include? word
+        caps << word.downcase
+      else
+        caps << word.capitalize
+      end
+    end
+    @title = caps.join(" ")
+
+  end
+
 end
