0f54d89aad7b4b426d44d6cd23a075c56611949e
Sam Nixa <spiznasm@gmail.com>
2016-11-25 20:56:25 -0600

    finished 04

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,14 @@
-#write your code here
+
+def translate(phrase)
+  new_phrase = ""
+  for word in phrase.split
+    if word.include? "qu"
+      parts = word.split("qu")
+      parts[0]+="qu"
+    else
+      parts = word.split(/([aeiou].*)/)
+    end
+    new_phrase << parts[1]+parts[0]+"ay "
+  end
+  return new_phrase[0..-2]
+end
